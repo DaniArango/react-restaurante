64abda305a4b47dc0ecb03d6b0a0ed2cf93061b3
[DaniArango] <[daniarangop93@gmail.com]>
2022-11-24 13:49:16 +0100

    Se implementan imagenes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import "./App.css";
+import './assets/bandeja.jpg'
+import foto from './assets/Gallo.png'
+import foto2 from './assets/bandeja.jpg'
+import foto3 from './assets/pupusas.jpg'
 
 
 function App() {
@@ -11,18 +15,21 @@ function App() {
       name: "Gallo Pinto",
       description: "El gallo pinto​ o gallopinto​ es un plato tradicional de Centroamérica que consiste en una combinación de arroz con frijoles ",
       price: 15,
+      src: foto
     },
     {
       id: 2,
       name: "Bandeja Paisa",
       description: "Tradicionalmente la bandeja paisa incluye un guiso de frijoles, arroz blanco, tocino o chicharrón, carne, chorizo, huevo frito, plátano maduro, aguacate y arepa. Sin embargo, vale destacar que la carne puede ser de res o de cerdo a la parrilla.",
       price: 20,
+      src: foto2
     },
     {
       id: 3,
       name: "Pupusas",
       description: "Tortilla gruesa de maíz o arroz hecha a mano que se rellena con uno o más ingredientes. Los ingredientes más comunes son el queso, el chicharrón, el ayote o los frijoles refritos.",
       price: 10,
+      src: foto3
     },
   ];
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,15 @@ import '../Home/Home.css'
 
 
 const Home = (props) => {
-    const platosLista = props.platos.map((dish) => {
+    const platosLista = props.platos?.map((plato) => {
+        console.log('hola',plato?.src)
         return (
             <div className="Home">
-               
-                Plato:<h2>{dish.name}</h2>
-                Descricion: <h2>{dish.description}</h2>
-                Precio: <h2>{dish.price}</h2>
+                <img src={plato.src} width="500px" height="400px" alt=""  />
+                <br />
+                Plato:<h2>{plato.name}</h2>
+                Descricion: <h2>{plato.description}</h2>
+                Precio: <h2>{plato.price}€</h2>
                 
             </div>
         );
